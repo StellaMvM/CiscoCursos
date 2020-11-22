6d03edf583886c4dec57a42f87bd9b291382e577
Miguel Velasquez Mamani (MvM) <39470479+StellaMvM@users.noreply.github.com>
2020-11-22 15:44:06 +0100

    agregando rutas 1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/inicio', function () {
+    return view('welcome');
+});
+Route::get('/ccna', function () {
+    return "estamos ccna";
+});
+Route::get('/ccnp', function () {
+    return "estamos en ccnp";
+});
+Route::get('/staff', function () {
+    return "estamos en staff";
+});
+Route::get('/galeria', function () {
+    return "estamos en galeria";
+});
+Route::get('/contacto', function () {
+    return "estamos en contacto";
+});
